30ca6d96a5152fdfeb4cc3be7374792f143e6f5a
Sabrina Mariha <sabrinammgs@gmail.com>
2024-06-30 12:53:45 -0300

    Migrations e repositorio responsáveis pela comunicação com o banco de dados MySQL do cadastro de médicos.

---
+++ src/main/java/br/com/sabrinamariha/med/voll/medico/MedicoRepository.java
@@ -0,0 +1,7 @@
+package br.com.sabrinamariha.med.voll.medico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+
+}
