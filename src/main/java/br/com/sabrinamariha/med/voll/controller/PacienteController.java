8e8f80be131b1bdbe708a277839d0f0017041a64
Sabrina Mariha <sabrinammgs@gmail.com>
2024-07-01 16:24:57 -0300

    Listagem de pacientes, com paginação de 10 pacientes por página e ordenado alfabeticamente pelos nomes.

--- src/main/java/br/com/sabrinamariha/med/voll/controller/PacienteController.java
+++ src/main/java/br/com/sabrinamariha/med/voll/controller/PacienteController.java
@@ -2,15 +2,17 @@ package br.com.sabrinamariha.med.voll.controller;
 
 
 import br.com.sabrinamariha.med.voll.paciente.DadosCadastroPaciente;
+import br.com.sabrinamariha.med.voll.paciente.DadosListagemPaciente;
 import br.com.sabrinamariha.med.voll.paciente.Paciente;
 import br.com.sabrinamariha.med.voll.paciente.PacienteRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -21,5 +23,10 @@ public class PacienteController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
         repository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 }
